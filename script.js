538bb711ff54c5037b48a2c0941de30bc7f88559
Patiencemugambi <patiencemugambi@gmail.com>
2023-06-12 07:02:25 +0300

    it will add smooth scrolling

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
